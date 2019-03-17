3d5abdac6ae0823b3a0f85e4a4dd7135f5f28d36
Michelan98 <michel.abdelnour16@gmail.com>
2019-03-17 19:37:03 -0400

    added line for getting the data for the Wifi class, line 30 in Lab5.java

--- src/ca/mcgill/ecse211/entryPoint/Lab5.java
+++ src/ca/mcgill/ecse211/entryPoint/Lab5.java
@@ -1,11 +1,12 @@
 package ca.mcgill.ecse211.entryPoint;
+
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 import ca.mcgill.ecse211.colorClassification.ColorClassification;
 import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.lcd.TextLCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
-
+import ca.mcgill.ecse211.WiFi.*;
 
 public class Lab5 {
 
@@ -13,8 +14,8 @@ public class Lab5 {
 	public static int RedTeam, GreenTeam, redCorner, greenCorner, Red_UR_x, Red_LLx, Red_UR_y,
 	Red_LL_y, Green_UR_x, Green_LL_x, Green_UR_y, Green_LL_y, Island_UR_x, Island_LL_x, Island_UR_y,
 	Island_LL_y, TNR_LL_x, TNR_UR_x, TNR_LL_y, TNR_UR_y, TNG_LL_x, TNG_UR_x, TNG_LL_y, TNG_UR_y, 
-	SZR_UR_x, SZR_UR_y, SZG_UR_x, SZG_UR_y, SZR_LL_y, SZR_LL_x, SZG_LL_x, SZG_LL_y; 	 
-
+	SZR_UR_x, SZR_UR_y, SZG_UR_x, SZG_UR_y, SZR_LL_y, SZR_LL_x, SZG_LL_x, SZG_LL_y; 	
+	
 	public static EV3LargeRegulatedMotor sensorMotor = new EV3LargeRegulatedMotor(LocalEV3.get().getPort("B"));
 	public static final TextLCD lcd = LocalEV3.get().getTextLCD();
 	public static final int TR = -1;  //0: red can, 1: green can, 2: yellow can, 3: blue can 
@@ -25,7 +26,8 @@ public class Lab5 {
 	 */
 	public static void main(String str[]) {
 		final ColorClassification colorClassification = new ColorClassification(sensorMotor, lcd, TR);
-
+		
+		WiFi.getData();
 		//System.out.println("initialize");
 		new Thread() {
 			public void run() {
